9dbd738cf8169911738895a174d5d34d59cbb3b5
Charles Ezan <charles.ezan1@uqac.ca>
2021-02-25 10:09:07 +0100

    Ajoutez des fichiers projet.

---
+++ sudoku/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sudoku
+{
+    public partial class Form1 : Form
+    {
+        Case[,] cases = new Case[10, 10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
