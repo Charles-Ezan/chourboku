ae9d2a93ea6acd4436329662c355b27c9e3b25a3
Charles Ezan <charles.ezan1@uqac.ca>
2021-02-25 10:32:41 +0100

    grid creation

--- sudoku/Form1.cs
+++ sudoku/Form1.cs
@@ -18,5 +18,33 @@ namespace sudoku
             InitializeComponent();
         }
 
+        //Creation de la grille
+        private void Start_Game(object sender, EventArgs e)
+        {
+            for(int i=0; i<10; i++)
+            {
+                grid.Rows.Add();
+            }
+
+            foreach (DataGridViewColumn column in grid.Columns)
+            {
+                column.Width = 80;
+            }
+
+            foreach (DataGridViewRow row in grid.Rows)
+            {
+                row.Height = 80;
+            }
+
+            for(int column=0; column < grid.Columns.Count; column++)
+            {
+                for (int row = 0; row < grid.Rows.Count; row++)
+                {
+                    Case new_case = new Case(grid[column, row]);
+                    cases[column, row] = new_case;
+                }
+            }
+        }
+
     }
 }
