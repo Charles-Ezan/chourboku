d7fc27abf43c48aec356ae0f0721bd8d4d6e2dbe
GuillaumeMontassier <52446347+GuillaumeMontassier@users.noreply.github.com>
2021-02-25 11:36:50 +0100

    agent

---
+++ sudoku/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudoku
+{
+    class Agent
+    {
+        public bool BacktrackingSearch()
+        {
+            return RecursiveBacktracking()
+        }
+
+        private bool RecursiveBacktracking()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
